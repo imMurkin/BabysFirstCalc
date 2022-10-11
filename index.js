c1ab69b184e70f0329570464422ed581c4c63532
imMurkin <inowhavethisemail@gmail.com>
2022-10-10 23:26:32 -0400

    added color pick and features

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const userInput1 = document.getElementById("userInput1")
+const userInput2 = document.getElementById("userInput2")
+const colorPicker = document.getElementById("color")
+
 function add(a, b) {
     // We want to call parseInt() on our a and b arguments, because we don't
     // know that they are numbers. 
@@ -6,12 +10,19 @@ function add(a, b) {
     return parseInt(a) + parseInt(b)
 }
 
-function submitAnswer() { 
-    // Get our input elements
-    // see also: https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-    const userInput1 = document.getElementById("userInput1")
-    const userInput2 = document.getElementById("userInput2")
+function subtract(a, b) {
+    return parseInt(a) - parseInt(b)
+}
 
+function multiply(a, b) {
+    return parseInt(a) * parseInt(b)
+}
+
+function divide(a, b) {
+    return parseInt(a) / parseInt(b)
+}
+
+function submitAnswer() { 
     // Check if input elements are null; return if so
     if (!userInput1 || !userInput2) return
     
@@ -24,7 +35,21 @@ function submitAnswer() {
     const value1 = userInput1.value
     const value2 = userInput2.value
 
-    const answer = add(value1, value2)
+    const optionAdd = document.getElementById("add")
+    const optionSub = document.getElementById("subtract")
+    const optionMul = document.getElementById("multiply")
+    const optionDiv = document.getElementById("divide")
+
+    let answer
+    if (optionDiv.checked) {
+        answer = divide(value1, value2)
+    } else if (optionSub.checked) {
+        answer = subtract(value1, value2)
+    } else if (optionMul.checked) {
+        answer = multiply(value1, value2)
+    } else {
+        answer = add(value1, value2)
+    }
 
     // Get our output element
     const result = document.getElementById("result")
@@ -44,7 +69,16 @@ submitButton.addEventListener("click", submitAnswer)
 
 // On key up, we try to call submit anwwer so users don't have to press the
 // button now -- an example of reactive input design
-const userInput1 = document.getElementById("userInput1")
-const userInput2 = document.getElementById("userInput2")
 userInput1.addEventListener("change", submitAnswer)
 userInput2.addEventListener("change", submitAnswer)
+
+function colorPick() {
+    // Be defensive!
+    if (!colorPicker) return
+
+    const colorValue = colorPicker.value
+    document.body.style.backgroundColor = colorValue
+}
+
+colorPicker.addEventListener("change", colorPick)
+
